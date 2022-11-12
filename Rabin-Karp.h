cdb73476b26e13d7c405f1f1b03fb33ad82ff445
IrinaPastushok <84584434+IrinaPastushok@users.noreply.github.com>
2022-11-12 20:49:48 +0300

    Create Rabin-Karp.h

---
+++ Rabin-Karp.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+using namespace std;
+void algoritm(const string cmp);
+long double hash_gorner(const string str, int len_of_range, int start, long double res_of_last_str);
+void output(const string str, int shift, int tmp, int len_of_range);
+
